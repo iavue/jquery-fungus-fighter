8d20105860c11d31b3a7613c9ec6b0602b93f76d
Ia Vue <iavue00@gmail.com>
2023-02-23 13:33:00 -0600

    was able to reduce HP and AP on click or arcanesceptre button

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let myAP = 100;
+
 function onReady() {
     
     // Make sure you check the index.html file! 
@@ -15,4 +18,45 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    // Add event handling for the 'arcane-sceptre' button
+    $('.attack-btn.arcane-sceptre').on('click', arcaneReduceHpAp);
+
+    // Add event handling for the 'entangle' button
+}
+
+function arcaneReduceHpAp() {
+    console.log('Inside arcane button');
+    fungusHP-=14;
+    myAP-=12;
+    console.log('Current fungus HP:', fungusHP);
+    console.log('Current myAP:', myAP);
+    // $('.hp-text').html(`
+    //     <div class="hp-text">${fungusHP} HP</div>
+    //  `);
+    // $('.ap-text').html(`
+    //     <div class="ap-text">${myAP} AP</div>
+    //  `);
+    // if (fungusHP === 2) {
+    //     fungusHP = 0;
+    // }
+    // if (myAP === 4) {
+    //     myAP = 0;
+    // }
+    render()
+}
+
+function render() {
+    $('.hp-text').html(`
+        <div class="hp-text">${fungusHP} HP</div>
+     `);
+    $('.ap-text').html(`
+        <div class="ap-text">${myAP} AP</div>
+     `);
+    if (fungusHP <= 0) {
+        fungusHP = 0;
+    }
+    if (myAP <= 0) {
+        myAP = 0;
+    }
 }
